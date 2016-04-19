aac96e3630928daad2ce543436a73b435d947d7e
EØJ <emil.johansen@gmail.com>
2016-04-19 16:06:50 +0200

    Parse user login functionality

--- app/src/main/java/com/helge/arrythmiamd/LoginActivity.java
+++ app/src/main/java/com/helge/arrythmiamd/LoginActivity.java
@@ -2,6 +2,14 @@ package com.helge.arrythmiamd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +17,43 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button bLogin = (Button) findViewById(R.id.bLogin);
+        final ProgressBar spinner = (ProgressBar) findViewById(R.id.progressBar);
+
+        assert bLogin != null;
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                assert etUsername != null;
+                assert etPassword != null;
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                assert spinner != null;
+                spinner.setVisibility(View.VISIBLE);
+                bLogin.setEnabled(false);
+
+
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            // Hooray! The user is logged in.
+                            Toast.makeText(LoginActivity.this, "JUHUU", Toast.LENGTH_SHORT).show();
+                        } else {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    spinner.setVisibility(View.GONE);
+                                    bLogin.setEnabled(true);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
     }
 }
