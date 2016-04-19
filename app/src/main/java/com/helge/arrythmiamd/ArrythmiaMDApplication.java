3df1a4bc49e9fddf9ed9fce236bc5f9b47d0a1c5
EØJ <emil.johansen@gmail.com>
2016-04-19 15:32:15 +0200

    Add Application file for Parse initialization

---
+++ app/src/main/java/com/helge/arrythmiamd/ArrythmiaMDApplication.java
@@ -0,0 +1,15 @@
+package com.helge.arrythmiamd;
+
+import com.parse.Parse;
+
+/**
+ * Created by emil on 19/04/16.
+ */
+public class ArrythmiaMDApplication extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(this, "7wop8ncD1stVBTcbPaCgDa1ZRHeiwkZulGYuV0nE", "RY7qjP9NJDwV81dEh3ZHVrMfy3ZU7qP9fKJ9QZOQ");
+    }
+}
