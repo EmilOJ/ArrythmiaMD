bbd489a0dd25c6ac6e8615942d495756e35ac8f7
lottetrap <lotte_trap@hotmail.com>
2016-04-19 16:57:47 +0200

    Choose patient by CPR-number

--- app/src/main/java/com/helge/arrythmiamd/ChoosePatientActivity.java
+++ app/src/main/java/com/helge/arrythmiamd/ChoosePatientActivity.java
@@ -9,5 +9,8 @@ public class ChoosePatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_patient);
+
+
+
     }
 }
